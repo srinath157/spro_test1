7d2241ed634dd36be76a2b30475be8134a05f81b
Srinath <spulumat@live.com>
2018-10-05 08:12:28 +0530

    updated

--- tree.py
+++ tree.py
@@ -1,25 +1,11 @@
-class Tree(dict):
-    def __missing__(self, key):
-        value = self[key] = type(self)()
-        return value
-
-
+# nested dictionary defined as function and dynamic class creation.
 def dict_fill(obj, key):
     obj[key] = type(obj)()
     return obj[key]
 
 
+# Dynamic class obj creation with name tree, inherited from dict class
 tree = type('_obj', (dict,), dict(__missing__=lambda self, key: dict_fill(self, key)))
+# OR
+tree1 = type('_obj', (dict,), dict(__missing__=dict_fill))
 
-x = tree()
-
-x['a']['b']['c'] = 1
-print(x)
-
-y = Tree()
-
-y['x']['y']['z'] = 'switch-name'
-
-print(y)
-
-print(x, y)
