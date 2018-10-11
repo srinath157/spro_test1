d9f8cf11799e188c188536614a3e832df90680a1
Srinath <spulumat@live.com>
2018-10-12 01:57:28 +0530

    updated

--- utils.py
+++ utils.py
@@ -2,20 +2,27 @@ class NestedDict(dict):
     """
     Nested Dictionary with Auto-initialize if the key is missing
     """
+
     def __missing__(self, key):
         value = self[key] = type(self)()
         return value
+
 
 # merge two nested dictionaries Dict and dict2
 def dict_merge(dict1, dict2):
     merged_dict = {}
     for k in dict1.keys() | dict2.keys():
         if k in dict1.keys() and k in dict2.keys():
-            if isinstance(dict1[k], dict) : merged_dict[k] = dict_merge(dict1[k], dict2[k])
-            else : merged_dict[k] = dict1[k]
-        elif k in dict1 : merged_dict[k] = dict1[k]
-        elif k in dict2 : merged_dict[k] = dict2[k]
+            if isinstance(dict1[k], dict):
+                merged_dict[k] = dict_merge(dict1[k], dict2[k])
+            else:
+                merged_dict[k] = dict1[k]
+        elif k in dict1:
+            merged_dict[k] = dict1[k]
+        elif k in dict2:
+            merged_dict[k] = dict2[k]
     return merged_dict
+
 
 # returns list with values matching a key in a nested dictionary. - here the key is 'switch'
 def get_switch_list(switchport_dict):
@@ -27,4 +34,3 @@ def get_switch_list(switchport_dict):
             if value:
                 switch_list.append(value)
     return list(set(switch_list))
-
